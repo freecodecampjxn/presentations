937684ba232814e4959a3cc67b7efdc75ef7bcb9
[Eliot Sanford] <eliot587@gmail.com>
2020-02-08 06:15:13 -0600

    prepped for content for js workshop

---
+++ 2020.02.08 javascript workshop/variables/app.js
@@ -0,0 +1,153 @@
+// comments
+
+/* shift + alt + a */
+
+// variable.. let is not block-scoped
+
+// console.log(variableName);
+
+// let example... let is block-scoped
+
+// const example... const is block-scoped
+
+// change, or overwrite, the value
+
+// value reassignment
+
+// declare a variable called cow
+
+// reassignment of cow
+
+// error will tell you it's not defined
+
+// camel case example
+
+// capitalization matters, plus an example of multiplication (*) using variables
+
+/**
+ * naming conventions for variables:
+ * - don't use reserved keywords, such as break, boolean, return
+ * - no numbers can start a variable
+ * - variables are case sensitive
+ * - underscores can be used, but they are typically not used at the beginning because other programming languages indicate private variables
+ *  */ 
+
+
+// example of payPerYear, payPerMonth, payPerWeek example
+
+
+// data types 
+
+// strings
+
+// 'you can't do this' is broken
+// "this 'works'" is good
+// insert the /' before the
+// number in the string
+
+// integers
+
+
+// log addition
+
+
+// math operations
+
+/*  plus: + // also the concatenation symbol
+    minus: -
+    times: *
+    divide: /
+    modulus: %
+    single equal sign(assignment operator): =
+*/
+
+
+// boolean
+
+
+// concatenation in quotes
+
+// concatenation in template literals
+
+
+//what do you think will output string + number
+
+
+//the number is coerced
+
+
+// the integer coerces the string in multiplication
+
+// increment "++"
+// decrement "--"
+
+// play the game what outputs in the log
+
+
+// variableName += int
+// vaiableName -= int
+
+// equivalent of ++
+// shorthand for int = int + int;
+
+
+
+// shorthand for int = int * int;
+
+// shorthand int = int / 5;
+
+
+
+
+// paid time off fixed rate of 5 hours per paycheck paid bi-weekly.
+// how many hours will you have accrued at the end of a year
+
+
+// psuedo code can help you problem solve step by step in English statements
+/**
+ * START
+ *  RECEIVE input = 5
+ *  SET thing = input
+ *  SET thingTimesFive = thing x 5
+ *  DISPLAY "Here is the input times 5: thingsTimesFive"
+ * END
+ * 
+ * pseudo code helps you avoid making spaghetti code
+ * 
+ * look up articles on pseudo code keywords
+ */
+
+// SCOPE
+
+// global
+
+
+//  var is global
+// let is block scoped
+
+ { // these brackets create an objects and local scope inside of it
+    // local scope
+    // let and const are block scoped
+
+    // reassignment attempt of const will error the const
+ }
+
+
+//  what will log for let const in the local scope?
+
+// this will error if uncommented
+
+//  naming something in a particular scope can stop you from poluting the global scope;
+
+
+// example where const cannot do this thing that var and let do
+
+
+
+
+/**
+ * start with what you know
+ * don't get stumped at what you can't do
+ */
+
+
